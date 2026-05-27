456d7df2e41ec9d45dccade85c454498163c1526
nevalhr <phneval@gmail.com>
2026-05-28 02:35:27 +0700

    feat : adding system MVP from the module and adding authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,19 @@
 <?php
 
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('todos', TodoController::class);
+// Public
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/todos', [TodoController::class, 'index']);
+Route::get('/todos/{todo}', [TodoController::class, 'show']);
+
+// Protected
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/todos', [TodoController::class, 'store']);
+    Route::put('/todos/{todo}', [TodoController::class, 'update']);
+    Route::delete('/todos/{todo}', [TodoController::class, 'destroy']);
+});
